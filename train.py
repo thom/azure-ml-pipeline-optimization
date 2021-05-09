6d0a22b802dc7a21746e7d51115af31a44d37a39
Thomas Weibel <thom@ikhono.net>
2021-05-09 15:54:58 +0000

    Complete training script

--- train.py
+++ train.py
@@ -10,19 +10,10 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+# Create TabularDataset using TabularDatasetFactory
+# Data is located at: https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv
+path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=path)
 
 def clean_data(data):
     # Dict for cleaning data
@@ -67,6 +58,14 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+# Clean data
+x, y = clean_data(ds)
+
+# Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+run = Run.get_context()
 
 if __name__ == '__main__':
     main()
