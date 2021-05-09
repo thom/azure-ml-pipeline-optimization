e4f19b0ce146c9c83a1e154935cecf8ff2c8a3a7
Thomas Weibel <thom@ikhono.net>
2021-05-09 18:56:51 +0200

    Fix bug
    Fix bug in training script: clean_data did not have any return values

--- train.py
+++ train.py
@@ -11,14 +11,17 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 # Create TabularDataset using TabularDatasetFactory
-# Data is located at: https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv
+# Data is located at: "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 ds = TabularDatasetFactory.from_delimited_files(path=path)
 
+
 def clean_data(data):
     # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    months = {"jan": 1, "feb": 2, "mar": 3, "apr": 4, "may": 5, "jun": 6,
+              "jul": 7, "aug": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12}
+    weekdays = {"mon": 1, "tue": 2, "wed": 3,
+                "thu": 4, "fri": 5, "sat": 6, "sun": 7}
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
@@ -37,24 +40,30 @@ def clean_data(data):
     x_df = x_df.join(education)
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+    x_df["poutcome"] = x_df.poutcome.apply(
+        lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--C', type=float, default=1.0,
+                        help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100,
+                        help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = LogisticRegression(
+        C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
@@ -62,8 +71,9 @@ def main():
 # Clean data
 x, y = clean_data(ds)
 
-# Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+# Split data into train and test sets
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.30, random_state=42)
 
 run = Run.get_context()
 
